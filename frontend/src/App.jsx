7f9f004f36d9097e962cc4bde89d20b2056906ed
Aditya <adityapanigrahy98@gmail.com>
2026-03-13 22:48:13 +0530

    tailwindcss setup, react-router-dom setup and some pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Navbar from './common/components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './features/home/pages/HomePage'
+import Movie from './features/movie/pages/Movie'
+import MovieDetail from './features/moviedetail/pages/MovieDetail'
+import SeatLayout from './features/seatdetails/pages/SeatLayout'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Routes>
+        <Route path='/' element= {<HomePage />} />
+        <Route path='/movies' element= {<Movie />} />
+        <Route path='/movies/:id' element= {<MovieDetail />} />
+        <Route path='/movies/:id/:date' element= {<SeatLayout />} />
+      </Routes>
     </>
   )
 }
