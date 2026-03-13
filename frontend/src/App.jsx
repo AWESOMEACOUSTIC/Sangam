35de443006138e62fbe3f5561b7e37cba75e3ddd
Aditya <adityapanigrahy98@gmail.com>
2026-03-13 23:00:31 +0530

    admin routing logic added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import Navbar from './common/components/Navbar'
-import { Route, Routes } from 'react-router-dom'
+import Footer from './common/components/Footer'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import HomePage from './features/home/pages/HomePage'
 import Movie from './features/movie/pages/Movie'
 import MovieDetail from './features/moviedetail/pages/MovieDetail'
 import SeatLayout from './features/seatdetails/pages/SeatLayout'
+import Favourites from './features/favourite/page/Favourites'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
+  
+  const isAdminRoute = useLocation().pathname.startsWith('/admin');
+  
   return (
     <>
-      <Navbar />
+    <Toaster />
+      { !isAdminRoute && <Navbar />}
       <Routes>
         <Route path='/' element= {<HomePage />} />
         <Route path='/movies' element= {<Movie />} />
         <Route path='/movies/:id' element= {<MovieDetail />} />
         <Route path='/movies/:id/:date' element= {<SeatLayout />} />
+        <Route path='/favourites' element= {<Favourites />} />
       </Routes>
+      { !isAdminRoute && <Footer /> }
     </>
   )
 }
