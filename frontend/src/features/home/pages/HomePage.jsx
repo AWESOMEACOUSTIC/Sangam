07bdf74515edb7644c96eada974c48c1a7436f7d
Aditya <adityapanigrahy98@gmail.com>
2026-03-17 12:45:33 +0530

    Add Fresh on Demand ranked carousel with sequenced numbers

--- frontend/src/features/home/pages/HomePage.jsx
+++ frontend/src/features/home/pages/HomePage.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import WhatToWatch from '../components/WhatToWatch'
+import React from "react";
+import Hero from "../components/Hero";
+import WhatToWatch from "../components/WhatToWatch";
+import FreshOnDemand from "../components/FreshOnDemand";
 
 function HomePage() {
   return (
-    <div className='bg-black min-h-screen'>
+    <div className="bg-black min-h-screen">
       <Hero />
       <WhatToWatch />
+      <FreshOnDemand />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
