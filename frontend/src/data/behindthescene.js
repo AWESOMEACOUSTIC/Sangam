9d97ed5b669b7528099a9d16f6216cf1ddcf25c0
Aditya <adityapanigrahy98@gmail.com>
2026-03-18 11:45:32 +0530

    Added behind the scenes data

---
+++ frontend/src/data/behindthescene.js
@@ -0,0 +1,72 @@
+const BTS_DATA = [
+  {
+    "id": 1,
+    "title": "Into The Spider-Verse",
+    "type": "movie",
+    "thumbnail": "https://i.pinimg.com/1200x/9f/7e/2e/9f7e2e5777d6d33212c0ef7f307f6f29.jpg",
+    "youtubeId": "l-wUKu_V2Lk"
+  },
+  {
+    "id": 2,
+    "title": "Dhurandhar",
+    "type": "movie",
+    "thumbnail": "https://i.pinimg.com/736x/d2/84/21/d28421688f49eb021c34347613c55611.jpg",
+    "youtubeId": "CpoJdnlST9o"
+  },
+  {
+    "id": 3,
+    "title": "Demimon Slayer: Kimetsu no Yaiba Infinity Castle",
+    "type": "tv",
+    "thumbnail": "https://img.youtube.com/vi/yQEondeGvKo/maxresdefault.jpg",
+    "youtubeId": "FLB_sLTgbPk"
+  },
+  {
+    "id": 4,
+    "title": "The Last of Us",
+    "type": "tv",
+    "thumbnail": "https://i.pinimg.com/1200x/b8/dc/bf/b8dcbfa401dc248219e9fab4e035b063.jpg",
+    "youtubeId": "THZwZti2jvo"
+  },
+  {
+    "id": 5,
+    "title": "Avatar: The Way of Water",
+    "type": "movie",
+    "thumbnail": "https://i.pinimg.com/736x/95/cd/cf/95cdcf9f1e493f88f6fa3eb7b0e3546a.jpg",
+    "youtubeId": "x_qm280q2e0"
+  },
+  {
+    "id": 6,
+    "title": "Breaking Bad",
+    "type": "tv",
+    "thumbnail": "https://i.pinimg.com/1200x/ab/36/40/ab36409c1701081ee1838556eee67e79.jpg",
+    "youtubeId": "Kj3QxYnymgI"
+  },
+  {
+    "id": 7,
+    "title": "Game of Thrones",
+    "type": "tv",
+    "thumbnail": "https://i.pinimg.com/1200x/ce/a6/71/cea671eca0d14db693e670ae648d5fb3.jpg",
+    "youtubeId": "JvIqP9ZSr7g"
+  },
+  {
+    "id": 8,
+    "title": "The Batman",
+    "type": "movie",
+    "thumbnail": "https://i.pinimg.com/736x/85/1e/a9/851ea95c30c1866d30516bf47a6816d9.jpg",
+    "youtubeId": "__aTCuCURPo"
+  },
+  {
+    "id": 9,
+    "title": "Interstellar",
+    "type": "movie",
+    "thumbnail": "https://img.youtube.com/vi/zSWdZVtXT7E/maxresdefault.jpg",
+    "youtubeId": "zSWdZVtXT7E"
+  },
+  {
+    "id": 10,
+    "title": "Money Heist",
+    "type": "tv",
+    "thumbnail": "https://img.youtube.com/vi/p_PJbmrX4uk/maxresdefault.jpg",
+    "youtubeId": "p_PJbmrX4uk"
+  }
+];
