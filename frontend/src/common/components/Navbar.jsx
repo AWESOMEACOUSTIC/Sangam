fe601a7c912bc03d22d2e75ff7c5ffe75eecbf52
Aditya <adityapanigrahy98@gmail.com>
2026-04-06 22:48:29 +0530

    fix navbar padding issue

--- frontend/src/common/components/Navbar.jsx
+++ frontend/src/common/components/Navbar.jsx
@@ -45,10 +45,10 @@ function Navbar() {
     <>
       <div
         className="fixed top-0 left-0 z-50 w-full flex items-center
-                   justify-between px-6 md:px-16 lg:px-36 py-5"
+                   justify-between px-6 md:px-16 lg:px-26"
       >
         <Link to="/" className="max-md:flex-1">
-          <img src={sangam_logo} alt="Logo" className="w-36 h-auto" />
+          <img src={sangam_logo} alt="Logo" className="w-36 h-36" />
         </Link>
         <div
           className={`max-md:absolute max-md:top-0 max-md:left-0
